62e81b11231233db950ba8757b925f12b66d0c06
Bryan Jonker <jonker@illinois.edu>
2024-08-29 17:08:12 -0500

    Fix JS error

--- .eleventy.js
+++ .eleventy.js
@@ -25,11 +25,11 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addFilter("generateGithubIssuesLink", function (name, components) {
     let returnValue = 'https://github.com/search?q=';
-    const arrayComponents = [];
+    const arrayComponents = [""];
     components.forEach(component => {
-        if (component.type === "web component" && !arrayComponents.includes(component.element-name)) {
-            returnValue += `repo%3Aweb-illinois%2F${component.element-name}+`;
-            arrayComponents.push(component.element-name);
+        if (component.type === "web component" && !arrayComponents.includes(component["element-name"])) {
+            returnValue += `repo%3Aweb-illinois%2F${component["element-name"]}+`;
+            arrayComponents.push(component["element-name"]);
         }
     });
     returnValue += 'state%3Aopen&type=Issues'
