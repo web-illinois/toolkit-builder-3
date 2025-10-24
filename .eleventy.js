a98f7b6604093b485af3e2d6c73797cda32fce91
Bryan Jonker <jonker@illinois.edu>
2025-10-24 13:03:25 -0500

    Adding testing_all_components.html file

--- .eleventy.js
+++ .eleventy.js
@@ -34,6 +34,37 @@ module.exports = function (eleventyConfig) {
     return returnValue;
   });
 
+  eleventyConfig.addFilter("outputComponent", function (name, component_version) {
+    let returnValue = '';
+    if (component_version['parent-style'] && component_version['parent-style'].length > 0) {
+      returnValue += `<div style="${component_version['parent-style']}">`;
+    } else {
+      returnValue += '<div>';
+    }
+    if (component_version['addon-html'] && component_version['addon-html'].length > 0) {
+      returnValue += component_version['addon-html'];
+    }
+    returnValue += '<' + component_version['element-name'];
+    if (component_version['attributes-fixed']) {
+      component_version['attributes-fixed'].forEach(attribute => {
+        returnValue += ` ${attribute.name}="${attribute.value}"`;
+      });
+    }
+    if (component_version['classes-fixed']) {
+      returnValue += ' class="';
+      component_version['classes-fixed'].forEach(attribute => {
+        returnValue += attribute.value + ' ';
+      });
+      returnValue += '" ';
+    }
+    returnValue += '>';
+    if (component_version.samples && component_version.samples.length > 0) {
+      returnValue += component_version.samples[0].text;
+    }
+    returnValue += '</' + component_version['element-name'] + '></div>';
+    return returnValue;
+  });
+
   eleventyConfig.addFilter("markdownify", (markdownString) =>
       markdownIt.render(markdownString)
   );
---
+++ site/all_components.md
@@ -0,0 +1,6 @@
+---
+data: component-versions
+permalink: "testing_all_components/index.html"
+layout: testing_components.liquid
+title: "Testing All Components"
+---
